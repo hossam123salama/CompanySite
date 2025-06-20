73ce2a1935d749e55541e97a488779f823262a92
hossam <hossam.222salama@gmail.com>
2025-06-20 15:42:45 +0300

    Initial commit with website files

---
+++ js/main.js
@@ -0,0 +1,29 @@
+// Scroll to top (لو عايز تضيفه)
+window.addEventListener('scroll', function () {
+  const topBtn = document.getElementById('scrollTopBtn');
+  if (topBtn) {
+    if (window.scrollY > 300) {
+      topBtn.style.display = 'block';
+    } else {
+      topBtn.style.display = 'none';
+    }
+  }
+});
+
+
+function hideFloatingButtons() {
+  const buttons = document.getElementById("floating-buttons");
+  if (buttons) {
+    buttons.style.display = "none";
+  }
+}
+
+ function showContactButtons() {
+    document.getElementById("contactButtons").style.display = "flex";
+    document.getElementById("toggleMessageBtn").style.display = "none";
+  }
+
+  function hideContactButtons() {
+    document.getElementById("contactButtons").style.display = "none";
+    document.getElementById("toggleMessageBtn").style.display = "block";
+  }
